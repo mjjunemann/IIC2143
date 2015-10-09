8af177daf4b2f9522c747a27f25a4e5e31029ad6
Matias <matiasjunemann@gmail.com>
2015-10-09 12:11:24 -0300

    Notification Center

---
+++ ChilExplox/src/Backend/NotificationCenter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Backend;
+import java.util.*;
+/**
+ *
+ * @author matia
+ */
+public class NotificationCenter 
+{
+  private Map<String,ArrayList<Notification>> notifications;
+  private ArrayList<Notification> displayed_notifations;
+  private Object lock;
+  
+  public void showNotification(Notification notification)
+  {
+  
+  }
+  
+  /**
+   * Show a group of notifications in the front end application in
+   * a notification center. 
+   * @param NotificationList Receives a list of notifications.
+   */
+  public void displayNotificationTipe(ArrayList<Notification> NotificationList)
+  {
+      for(Notification not: NotificationList)
+      {
+          
+      }
+  }
+}
