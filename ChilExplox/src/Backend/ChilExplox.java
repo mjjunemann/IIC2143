c3ba74b6653238bb299983d9322b3face0de3b9c
fdoflorenzano <faflorenzano@uc.cl>
2015-10-12 05:56:50 -0300

    Agregada logica de ChilExplox de login, agregado de sucursales y usuarios. Inicializa sus atributos, agregado NotificationCenter, ahora mapea username al usuario, tiene una lista de las direcciones de sucursales y un Diccionario de direccion a las sucursales para almacenar.

--- ChilExplox/src/Backend/ChilExplox.java
+++ ChilExplox/src/Backend/ChilExplox.java
@@ -14,31 +14,40 @@ import java.util.*;
 public class ChilExplox implements java.io.Serializable
 {
     private static ChilExplox instance;
-    private Map<String,String> Usuarios;
-    private ArrayList<String> subsidiaries_addrs;
+    private Map<String,User> users;
+    private ArrayList<Address> subsidiaries_addrs;
+    private Map<Address,Subsidiary> subsidiaries;
     private Subsidiary current_subsidiary;
     private final Messaging messaging;
+    private NotificationCenter notificationcenter;
     
     protected ChilExplox()
     {
-        messaging = new Messaging();
-        
+        this.messaging = new Messaging();
+        this.users = new HashMap();
+        this.subsidiaries = new HashMap();
+        this.subsidiaries_addrs = new ArrayList<>();
+        this.notificationcenter = new NotificationCenter();
         // Here could be a static class that take cares of loading the information
         //loadSubsidaries
         //loadUsers
         //loadInformation
         //loadTrucks
     }
-    
     /**
      * Validate user information
      *
      * @param username 
      * @param password 
      */
-    public void login(String username, Address subsidary_addrs)
+    public boolean login(String username, String password, Address subsidiary_addrs)
     {
-        
+        if( this.subsidiaries_addrs.contains(subsidiary_addrs) 
+                && users.get(username).getPassword() == password){
+            this.current_subsidiary = this.subsidiaries.get(subsidiary_addrs);
+            return true;
+            }
+        return false;
     }
     
     /**
@@ -58,6 +67,19 @@ public class ChilExplox implements java.io.Serializable
     public void addSubsidary(Address address)
     {
         Subsidiary s = new Subsidiary(address);
-        
+        this.subsidiaries_addrs.add(address);
+        this.subsidiaries.put(address,s);
+    }
+    public boolean addUser(String username, String name, String password)
+    {
+        if(!this.users.containsKey(username)){
+            User u = new User(username,name,password);
+            this.users.put(username,u);
+            return true;
+        }
+        return false;
+    }
+    public ArrayList<Address> getSubsidiariesAddress(){
+        return this.subsidiaries_addrs;
     }
 }
