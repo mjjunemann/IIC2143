a65248fb3f8dc9b9c955a94c2901c3d7ecc784af
fdoflorenzano <faflorenzano@uc.cl>
2015-10-22 18:56:57 -0300

    empezando visión de camiones, agrego imagen de camion

--- ChilExplox/src/Frontend/WatchTrucksListFXMLController.java
+++ ChilExplox/src/Frontend/WatchTrucksListFXMLController.java
@@ -17,7 +17,12 @@ import javafx.scene.control.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.cell.ComboBoxListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.shape.Box;
 import javafx.stage.Stage;
 /**
  * FXML Controller class
@@ -32,7 +37,13 @@ public class WatchTrucksListFXMLController implements Initializable, iController
     @FXML
     private Button returnButton;
     @FXML
-    private ListView<String> trucksListView; 
+    private TilePane arrivedTruckTile;
+    @FXML
+    private TilePane ownTruckTile;
+    @FXML
+    private AnchorPane gridAnchor;
+    /*@FXML
+    private ListView<String> trucksListView;*/
     /**
      * Initializes the controller class.
      */
@@ -66,8 +77,30 @@ public class WatchTrucksListFXMLController implements Initializable, iController
             }
         }
         
-        trucksListView.setItems(trucksStringList);
-
+        /*trucksListView.setItems(trucksStringList);*/
+        Image im;
+        ImageView imV ;
+        for (int i=0; i < 3 ; i++){
+            im = new Image("images/truck.png");
+            imV = new ImageView();
+            imV.setImage(im);
+            imV.setFitWidth(75);
+            imV.setPreserveRatio(true);
+            imV.setSmooth(true);
+            imV.setCache(true);
+            arrivedTruckTile.getChildren().add(imV);
+        }
+        for (int i=0; i < 30 ; i++){
+            im = new Image("images/truck.png");
+            imV = new ImageView();
+            imV.setImage(im);
+            imV.setFitWidth(75);
+            imV.setPreserveRatio(true);
+            imV.setSmooth(true);
+            imV.setCache(true);
+            ownTruckTile.getChildren().add(imV);
+        }
+        
     }
     
 }
