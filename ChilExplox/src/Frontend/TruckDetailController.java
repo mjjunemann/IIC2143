fb50af9c39c9cf81332d7172b537dc73ba506e36
Matias <matiasjunemann@gmail.com>
2015-10-23 15:39:44 -0300

    encomiendas truck

--- ChilExplox/src/Frontend/TruckDetailController.java
+++ ChilExplox/src/Frontend/TruckDetailController.java
@@ -5,12 +5,17 @@
  */
 package Frontend;
 
+import Backend.Parcel;
 import Backend.Truck;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -22,14 +27,37 @@ public class TruckDetailController implements Initializable, iController {
     
     ChilExploxApp main;
     Truck truck;
-    
+    private ObservableList<Parcel> parcels;
+    //<editor-fold desc="FXML">
+    @FXML
+    private TableView<Parcel> truckContent;
+    @FXML
+    private TableColumn<Parcel,String> parcel_id;
+    @FXML
+    private TableColumn<Parcel,Float> volume;
+    @FXML
+    private TableColumn<Parcel,Float> weight;
+    @FXML
+    private TableColumn<Parcel,String> destination;
+    //</editor-fold>
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        initTableView();
+    }
+    public void initTableView()
+    {
+        parcel_id.setCellValueFactory(i->i.getValue().idProperty());
+        volume.setCellValueFactory(i->i.getValue().volumeProperty().asObject());
+        weight.setCellValueFactory(i->i.getValue().weightProperty().asObject());
+        // FALTA DESTINATION
+        
+        parcels = FXCollections.observableArrayList(Parcel.extractor());
+        truckContent.setItems(parcels);
+
+    }
     @Override
     public void setChilExploxApp(ChilExploxApp main){
         this.main = main;
@@ -37,6 +65,7 @@ public class TruckDetailController implements Initializable, iController {
     
     public void setTruck(Truck truck){
         this.truck = truck;
+        this.truck.getParcels().stream().forEach(p-> parcels.add(p));
     }
     
     @FXML
