48c457d37fd45de124aaf5313e92a3d3538cc079
fdoflorenzano <faflorenzano@uc.cl>
2015-11-04 14:40:25 -0300

    Más avance en camiones, botones conectados y eventos bien armados.

---
+++ ChilExplox/src/Frontend/TruckImage.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Frontend;
+
+import Backend.State;
+import Backend.Truck;
+import javafx.event.EventHandler;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ *
+ * @author Fernando
+ */
+public class TruckImage {
+    
+    final Truck truck;
+    Image img;
+    final ImageView view;
+    final WatchTrucksListFXMLController controller;
+    
+    public TruckImage(Truck truck, WatchTrucksListFXMLController c){
+        this.truck=truck;
+        this.controller = c;
+        updateImage();
+        this.view = new ImageView(this.img);
+        this.view.setOnMouseClicked((MouseEvent t) -> {
+            controller.plateLabel.setText(truck.getPlate());
+            controller.stateLabel.setText(truck.getAvaibility().toString());
+            controller.destinationLabel.setText(truck.getDestinyString());
+            controller.nParcelsLabel.setText(String.valueOf(truck.getParcels().size()));
+            controller.selectedTruck = truck;
+        });
+    }
+    public void updateImage(){
+        if (this.truck.getAvaibility() == State.Origin) {
+            img = new Image("images/truck.png",120,120,false,false);
+        }
+        else{
+            img = new Image("images/red_truck.png",120,120,false,false);
+        }
+    }
+}
--- ChilExplox/src/Frontend/WatchTrucksListFXMLController.java
+++ ChilExplox/src/Frontend/WatchTrucksListFXMLController.java
@@ -21,9 +21,7 @@ import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.cell.ComboBoxListCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
@@ -44,16 +42,8 @@ import javafx.util.Callback;
  * @author guillermofigueroa
  */
 public class WatchTrucksListFXMLController implements Initializable, iController {
-
-
     
     ChilExploxApp main;
-    @FXML
-    private Button backButton;
-    @FXML
-    private Button localTruckButton;
-    @FXML
-    private Button arrivedTruckButton;
     @FXML
     private Button viewContentButton;
     @FXML
@@ -63,164 +53,120 @@ public class WatchTrucksListFXMLController implements Initializable, iController
     @FXML
     private Button backTruckButton;
     @FXML
-    private TableView<Truck> truckTable;
-    @FXML
-    private TableColumn<Truck,String> idColumn;
-    @FXML
-    private TableColumn<Truck,String> stateColumn;
-    @FXML
-    private TableColumn<Truck,Number> numberColumn;
-    @FXML
-    private TableColumn<Truck,String> destinyColumn;
-    @FXML
     private TilePane TruckTile;
     @FXML
-    private TextArea truckInfo;
+    public Label plateLabel;
+    @FXML
+    public Label stateLabel;
+    @FXML
+    public Label destinationLabel;
+    @FXML
+    public Label nParcelsLabel;
+    @FXML
+    private Label truckTitle;
+    
     private boolean muestraLocal = true;
+    public Truck selectedTruck;
+    private TruckImage[] localTrucksImgs;
+    private TruckImage[] arrivedTrucksImgs;
     
-    
-    private ObservableList<Truck> transportData = FXCollections.observableArrayList();
-    /*@FXML
-    private ListView<String> trucksListView;*/
-    /**
-     * Initializes the controller class.
-     */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void initialize(URL url, ResourceBundle rb) {}    
     
     @Override
     public void setChilExploxApp(ChilExploxApp main){
         this.main = main;
         initializeListView();
     }
-
     @FXML
-    private void returnToSubsidary(ActionEvent event) {
-        main.changeScene("SubsidiaryViewFXML.fxml", SubsidiaryViewFXMLController.class);
-    }
+    private void returnToSubsidary(ActionEvent event) { main.changeScene("SubsidiaryViewFXML.fxml", SubsidiaryViewFXMLController.class); }
+    
     private void initializeListView(){
-        Map<String,ITransport> transports = 
-                this.main.getChilExplox().getCurrentSubsidiary().getVehicles();
-        for (String key: transports.keySet()){
-            Truck transport = (Truck) transports.get(key);
-            transportData.add(transport);
-        }
-        idColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getPlate()));
-        stateColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getState().toString()));
-        numberColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty( cellData.getValue().getParcels().size()));
-        destinyColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDestinyString()));
-        
-        truckTable.setItems(transportData);
-        
         TruckTile.setHgap(5);
         TruckTile.setVgap(5);
-        TruckTile.setPrefRows(3);
-        Image im = new Image("images/truck.png",120,120,false,false);
-        final ImageView m = new ImageView(im);
-        m.setOnMouseEntered(new EventHandler<MouseEvent>(){
-              @Override
-              public void handle(MouseEvent t){
-                  truckInfo.appendText("Buena!\n");
-                  truckInfo.setLayoutX(100);
-              }
-            });
-        m.setOnMouseExited(new EventHandler<MouseEvent>(){
-              @Override
-              public void handle(MouseEvent t){
-                  truckInfo.appendText("Hola!\n");
-                  truckInfo.setLayoutX(50);
-              }
-            });
-        for (int i = 0; i < 1; i++) {
-            TruckTile.getChildren().add(m);
+        selectedTruck = null;
+        
+        Map<String,ITransport> local = 
+                this.main.getChilExplox().getCurrentSubsidiary().getVehicles();
+        ArrayList<ITransport> arrived = 
+                this.main.getChilExplox().getCurrentSubsidiary().getArrivedVehicles();
+        
+        localTrucksImgs = new TruckImage[local.size()];
+        arrivedTrucksImgs = new TruckImage[arrived.size()];
+        
+        int index = 0;
+        for (String key: local.keySet()){
+            Truck transport = (Truck) local.get(key);
+            localTrucksImgs[index] = new TruckImage(transport,this);
+            index += 1;
         }
+        for (ITransport t: arrived){
+            arrivedTrucksImgs[index] = new TruckImage((Truck) t,this);
+            index += 1;
+        }
+        loadLocalTrucks(new ActionEvent());
 
     }   
     @FXML
     private void loadLocalTrucks(ActionEvent event ){
-        transportData = FXCollections.observableArrayList();
-        Map<String,ITransport> transports = 
-                this.main.getChilExplox().getCurrentSubsidiary().getVehicles();
-        for (String key: transports.keySet()){
-            Truck transport = (Truck) transports.get(key);
-            transportData.add(transport);
+        TruckTile.getChildren().clear();
+        for (int index = 0 ; index < localTrucksImgs.length ; index++){
+            TruckTile.getChildren().add(localTrucksImgs[index].view);
+            index += 1;
         }
-        idColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getPlate()));
-        stateColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getState().toString()));
-        numberColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty( cellData.getValue().getParcels().size()));
-        destinyColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDestinyString()));
-        
-        truckTable.setItems(transportData);
-        
-        muestraLocal = !muestraLocal;
+        muestraLocal = true;
+        clearLabels();
     }
     @FXML
     private void loadArrivedTrucks(ActionEvent event ){
-        transportData = FXCollections.observableArrayList();
-        ArrayList<ITransport> transports = 
-                this.main.getChilExplox().getCurrentSubsidiary().getArrivedVehicles();
-        for (ITransport transport: transports){
-            Truck truck = (Truck) transport;
-            transportData.add(truck);
+        TruckTile.getChildren().clear();
+        for (int index = 0 ; index < arrivedTrucksImgs.length ; index++){
+            TruckTile.getChildren().add(arrivedTrucksImgs[index].view);
+            index += 1;
         }
-        idColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getPlate()));
-        stateColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getState().toString()));
-        numberColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty( cellData.getValue().getParcels().size()));
-        destinyColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDestinyString()));
-        
-        truckTable.setItems(transportData);
-        
-        muestraLocal = !muestraLocal;
-        
+        muestraLocal = false;
+        clearLabels();
     }
+    
+    private void clearLabels(){
+        if (muestraLocal) {
+            truckTitle.setText("Local Trucks");
+            viewContentButton.setVisible(true);
+            loadParcelButton.setVisible(true);
+            unloadParcelButton.setVisible(false);
+            backTruckButton.setVisible(false);
+        }else{
+            truckTitle.setText("Arrived Trucks");
+            viewContentButton.setVisible(false);
+            loadParcelButton.setVisible(false);
+            unloadParcelButton.setVisible(true);
+            backTruckButton.setVisible(true);
+        }
+        plateLabel.setText("-");
+        stateLabel.setText("-");
+        destinationLabel.setText("-");
+        nParcelsLabel.setText("-");
+        selectedTruck = null;
+    }
+    
     @FXML
     private void seeContent(ActionEvent event){
-        int index = truckTable.getSelectionModel().getSelectedIndex();
-        if (muestraLocal && index >-1) {
-           int cont = 0;
-           Truck truck = null;
-           for( String key: main.getChilExplox().getCurrentSubsidiary().getVehicles().keySet() ){
-               if (index == cont) {
-                   truck = (Truck) main.getChilExplox().getCurrentSubsidiary().getVehicles().get(key);
-                   break;
-               }
-               cont+=1;
-           }
-            changeSceneToTruck(truck);
+        
+        if (selectedTruck != null) {
+            changeSceneToTruck(selectedTruck);
         }
     }
     @FXML
     private void loadParcel(ActionEvent event){
-        int index = truckTable.getSelectionModel().getSelectedIndex();
-        if (muestraLocal&& index >-1) {
-            int cont = 0;
-           Truck truck = null;
-           for( String key: main.getChilExplox().getCurrentSubsidiary().getVehicles().keySet() ){
-               if (index == cont) {
-                   truck = (Truck) main.getChilExplox().getCurrentSubsidiary().getVehicles().get(key);
-                   break;
-               }
-               cont+=1;
-           }
-           changeSceneToLoadParcel(truck);
+        
+        if (selectedTruck != null) {
+           changeSceneToLoadParcel(selectedTruck);
         }
     }
     @FXML
     private void unloadParcel(ActionEvent event){
-        int index = truckTable.getSelectionModel().getSelectedIndex();
-        if (!muestraLocal&& index >-1) {
-           int cont = 0;
-           Truck truck = null;
-           for( String key: main.getChilExplox().getCurrentSubsidiary().getVehicles().keySet() ){
-               if (index == cont) {
-                   truck = (Truck) main.getChilExplox().getCurrentSubsidiary().getVehicles().get(key);
-                   break;
-               }
-               cont+=1;
-           }
-           changeSceneToUnloadParcel(truck);
+        if (selectedTruck != null) {
+           changeSceneToUnloadParcel(selectedTruck);
         }
     }
     @FXML
