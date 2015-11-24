d10c8e1f1b25686a164f4aa9644c704c2dd47aa6
fdoflorenzano <faflorenzano@uc.cl>
2015-11-24 15:36:48 -0300

    Historial de pedido lsito en frontend.

--- ChilExplox/src/Frontend/SaleReportsFXMLController.java
+++ ChilExplox/src/Frontend/SaleReportsFXMLController.java
@@ -5,9 +5,16 @@
  */
 package Frontend;
 
+import Backend.ArchiveType;
 import Backend.Record;
+import Backend.Report;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -15,6 +22,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
 /**
  * FXML Controller class
@@ -24,6 +33,7 @@ import javafx.scene.control.ListView;
 public class SaleReportsFXMLController implements Initializable, iController {
 
     ChilExploxApp main;
+    private Desktop desktop = Desktop.getDesktop();
     String option;
     
     @FXML
@@ -34,6 +44,9 @@ public class SaleReportsFXMLController implements Initializable, iController {
      Label titleLabel;
     @FXML
      ListView<Record> reportListView;
+    
+    final FileChooser fileChooser = new FileChooser();
+    final DirectoryChooser directoryChooser = new DirectoryChooser();
     
     /**
      * Initializes the controller class.
@@ -70,5 +83,29 @@ public class SaleReportsFXMLController implements Initializable, iController {
     @FXML
     public void backScene(){
         this.main.changeScene("ViewReportsFXML.fxml", ViewReportsFXMLController.class);
+    }
+    
+    @FXML
+    public void generatePDF(){
+        
+        File f = this.directoryChooser.showDialog(this.main.getStage());
+        if (f != null) {
+            String filepath = f.getPath()+"/report.pdf";
+            System.out.print(f.getPath());
+            Report r = new Report(ArchiveType.Delivery,filepath);
+            File fil = new File(filepath);
+            openFile(fil);
+        }
+    }
+    
+    private void openFile(File file) {
+        try {
+            desktop.open(file);
+        } catch (IOException ex) {
+            Logger.getLogger(
+                SaleReportsFXMLController.class.getName()).log(
+                    Level.SEVERE, null, ex
+                );
+        }
     }
 }
